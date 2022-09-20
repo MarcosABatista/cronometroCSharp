1798daf5304cf3a33c7b107c0a78c2e954b9c91f
Marcos de Araujo Batista <83717885+MarcosABatista@users.noreply.github.com>
2022-09-20 12:28:39 -0300

    Add files via upload

---
+++ Cronometro/Program.cs
@@ -0,0 +1,44 @@
+﻿Menu();
+
+static void Menu()
+{
+  Console.Clear();
+  Console.WriteLine("S = Segundo");
+  Console.WriteLine("M = Minuto");
+  Console.WriteLine("0 = Sair");
+  Console.WriteLine("Quanto tempo deseja contar?");
+
+  string data = Console.ReadLine().ToLower();
+  char type = char.Parse(data.Substring(data.Length - 1, 1));
+  int time = int.Parse(data.Substring(0, data.Length - 1));
+  if (type != 's' & type != 'm')
+  {
+    Console.WriteLine("Opção invalida!");
+  }
+  else
+  {
+    int multiplicador = 1;
+    if (type == 'm')
+      multiplicador = 60;
+
+    start(time * multiplicador);
+  }
+  Menu();
+}
+static void start(int time)
+{
+  int currentTime = 0;
+  while (currentTime != time)
+  {
+    Console.Clear();
+    currentTime++;
+    Console.WriteLine(currentTime);
+    Thread.Sleep(1000);
+
+  }
+  Console.Clear();
+  System.Console.WriteLine("Cronometro finalizado!");
+  Thread.Sleep(2000);
+  Menu();
+
+}
